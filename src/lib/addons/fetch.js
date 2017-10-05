b33f2ffbf85b60ea63cd31ab2836de3ac0c21bce
fln <florian.lebrun.pv1@gmail.com>
2017-10-05 14:20:43 +0200

    chore: fix fetch

--- src/lib/addons/fetch.js
+++ src/lib/addons/fetch.js
@@ -19,11 +19,7 @@ export default {
         throw new Error("no endpoint for " + url)
       }
 
-      const request = {
-        method: "GET",
-        headers: { "Accept": "application/json" },
-      }
-
+      const request = Object.assign({ headers:{} }, options)
       if (options) {
         if (options.body instanceof Object) {
           request.headers["Content-Type"] = "application/json"
@@ -33,8 +29,12 @@ export default {
           const contentType = options.headers["Content-Type"]
           if (!contentType) request.headers["Content-Type"] = "text/plain"
         }
-        if (options.method) request.method = options.method
-        if (options.headers) Object.assign(request.headers, options.headers)
+        request.method = options.method || "GET"
+        request.headers = Object.assign({ "Accept": "application/json" }, options.headers)
+      }
+      else {
+        request.method = "GET"
+        request.headers = { "Accept": "application/json" }
       }
 
       const response = (res) => {
@@ -48,6 +48,9 @@ export default {
           }
 
           if (res.status >= 200 && res.status < 300) {
+            if (endpoint.feedback) {
+              endpoint.feedback(res, data)
+            }
             if (res.status === 204) {
               return respondSuccess({})
             }
@@ -55,7 +58,7 @@ export default {
               return res.json().then(respondSuccess, respondError)
             }
           }
-          else if (res.status === 401 && hasLoginRecovery) {
+          else if (res.status === 401 && hasLoginRecovery && endpoint.login) {
             if (!this.loginPromise) {
               this.loginPromise = endpoint.login(this.application)
               if (!this.loginPromise) throw new Error()
@@ -71,11 +74,14 @@ export default {
               respondError(error)
             })
           }
-          else {
+          else if (res.json && res.json instanceof Function) {
             return res.json().then(respondError, () => {
               this.enableLoginRecovery = false
               respondError(res.error)
             })
+          }
+          else {
+            return respondError(res.error)
           }
         })
       }
